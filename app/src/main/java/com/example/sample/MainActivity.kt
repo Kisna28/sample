eb14a2201b04cbb8ee557c1bd787674d51dd6671
Kisna <kisnapatel28@gmail.com>
2024-02-19 16:20:52 +0530

    second commit

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.sample
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, SignIn_activity()) // Replace SignInFragment() with your actual fragment class
-            .commit()
+        val signup = findViewById<TextView>(R.id.signin_notaccount)
+        val signup1 = findViewById<TextView>(R.id.sign_up)
+
+        signup.setOnClickListener{
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+        signup1.setOnClickListener{
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+
     }
     }
--- app/src/main/java/com/example/sample/SignUp.kt
+++ app/src/main/java/com/example/sample/SignUp.kt
@@ -1,11 +1,26 @@
 package com.example.sample
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUp : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val signin=findViewById<TextView>(R.id.signup_already)
+        val signin1=findViewById<TextView>(R.id.sign_in)
+        signin.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+        signin1.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
