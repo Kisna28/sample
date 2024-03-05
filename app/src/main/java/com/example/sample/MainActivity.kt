590d16536a3a2104dd03b932067e76abacdf47b4
Kisna <kisnapatel28@gmail.com>
2024-03-05 11:28:10 +0530

    add firebase remote config

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -4,7 +4,14 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -14,15 +21,45 @@ class MainActivity : AppCompatActivity() {
 
         val signup = findViewById<TextView>(R.id.signin_notaccount)
         val signup1 = findViewById<TextView>(R.id.sign_up)
+        val login = findViewById<Button>(R.id.signin_btn1)
 
-        signup.setOnClickListener{
-            val intent = Intent(this,SignUp::class.java)
+        signup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
-        signup1.setOnClickListener{
-            val intent = Intent(this,SignUp::class.java)
+        signup1.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
 
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d("Change", "Config params updated: $updated")
+                    val text = remoteConfig.getString("login_submit_text")
+                    login.text = text
+                    Toast.makeText(
+                        this,
+                        "Fetch and activate succeeded",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Fetch failed",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+
     }
-    }
+}
+
